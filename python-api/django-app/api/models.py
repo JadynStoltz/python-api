52228acb78eb610af55f07e724fde28ee82ba277
Jadyn Stoltz <jstoltz@dearx.co.za>
2025-07-18 09:24:14 +0200

    Added comments

--- python-api/django-app/api/models.py
+++ python-api/django-app/api/models.py
@@ -9,6 +9,7 @@ class User(models.Model):
     def __str__(self):
         return self.name
     
+# Employee model to store employee details
 class Employee(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
